7b6f810530a7e9c4b55fa202517549f1fb2755d7
EvgeniiMorozov <emorozov1101@gmail.com>
2021-03-14 13:10:42 +0500

    Add mul and div
    Mul and div operations and there operator`s processing

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ def sub_nums(a, b):
     return a - b
 
 
+def mul_nums(a, b):
+    return a * b
+
+
+def div_nums(a, b):
+    return a / b
+
+
 def main():
     num1 = int(input('Введите первое число: '))
     num2 = int(input('Введите второе число: '))
@@ -15,6 +23,10 @@ def main():
         print(sum_nums(num1, num2))
     elif operator == '-':
         print(sum_nums(num1, num2))
+    elif operator == '*':
+        print(mul_nums(num1, num1))
+    elif operator == '/':
+        print(div_nums(num1, num2))
 
 
 if __name__ == '__main__':
