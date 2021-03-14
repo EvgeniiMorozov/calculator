89479f45bd160d52986f627a94b79d670cd779ad
EvgeniiMorozov <emorozov1101@gmail.com>
2021-03-14 13:03:36 +0500

    Add sub function
    Sub func and operator '-' processing

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ def sum_nums(a: int, b: int) -> int:
     return a + b
 
 
+def sub_nums(a, b):
+    return a - b
+
+
 def main():
     num1 = int(input('Введите первое число: '))
     num2 = int(input('Введите второе число: '))
     operator = input('Введите операцию: ')
 
     if operator == '+':
+        print(sum_nums(num1, num2))
+    elif operator == '-':
         print(sum_nums(num1, num2))
 
 
